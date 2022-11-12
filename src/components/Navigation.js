e16be1691416d979ed14e146e79d5539232ac489
Tran Phu Son <phuson2809@gmail.com>
2022-11-12 10:21:54 +0700

    login

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,105 +1,112 @@
 import React, { useEffect, useState } from "react";
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { ThemeContext } from "./ThemeContext";
-import { useLocation } from "react-router-dom";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Container,
+  Button,
+} from "@mui/material";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import { NavLink } from "react-router-dom";
 import LoginGoogle from "./LoginGoogle";
-// import { Navbar, NavItem, Icon } from 'react-materialize'
 
-export default function Navigation({ loading, setLoading }) {
-  //assigning location variable
-  const location = useLocation();
-  //destructuring pathname from location --> /contact
-  const { pathname } = location;
-  //split method to get the name of the path in array --> ['', 'contact']
-  const splitLocation = pathname.split("/");
-  const { theme, toggle, dark } = useContext(ThemeContext);
+function Navigation({ loading, setLoading }) {
   const [isLogin, setIsLogin] = useState(null);
-  useEffect(
-    () => {
-      setLoading(false);
-      setIsLogin(JSON.parse(localStorage.getItem("userLogin")));
-    },
-    // eslint-disable-next-line
-    [loading]
-  );
-  return (
-    <header>
-      <div
-        className="header"
-        style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
-      >
-        <div className="headerLogo">FILMS</div>
-        <div className="navbar">
-          <nav>
-            <ul className="nav-cont">
-              {isLogin && (
-                <li>
-                  <Link className="active" to={"/addFilm"}>
-                    ADD
-                  </Link>
-                </li>
-              )}
 
-              <li>
-                <Link className="active" to={""}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to={"news"}>News</Link>
-              </li>
-              <li>
-                <Link to={"about"}>About</Link>
-              </li>
-              <li>
-                <Link to={"contact"}>Contact</Link>
-              </li>
-              {isLogin ? (
-                <>
-                  <li>
-                    <Link
-                      to="#"
-                      onClick={() => {
-                        localStorage.removeItem("userLogin");
-                        setLoading(true);
-                      }}
-                    >
-                      LOGOUT
-                    </Link>
-                  </li>
-                  <img
-                    src={isLogin.imageUrl}
-                    alt=""
-                    style={{
-                      width: "50px",
-                      height: "50px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                </>
-              ) : (
-                <LoginGoogle setLoading={setLoading} />
-              )}
-            </ul>
-          </nav>
-        </div>
-        <div style={{ position: "relative" }}>
-          <a
-            className="switch-mode"
-            href="#"
-            onClick={toggle}
-            style={{
-              backgroundColor: theme.backgroundColor,
-              color: theme.color,
-              outline: "none",
+  useEffect(() => {
+    setLoading(false);
+    setIsLogin(JSON.parse(localStorage.getItem("userLogin")));
+  }, [loading]);
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <NewspaperIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
-            data-testid="toggle-theme-btn"
           >
-            Switch Nav {!dark ? "On" : "Off"}
-          </a>
-        </div>
-      </div>
-    </header>
+            NEWS
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <NavLink style={{ textDecoration: "none" }}>
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                Home
+              </Button>
+            </NavLink>
+            <NavLink style={{ textDecoration: "none" }}>
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                Top News
+              </Button>
+            </NavLink>
+            <NavLink style={{ textDecoration: "none" }}>
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                Contact
+              </Button>
+            </NavLink>
+            {isLogin && (
+              <NavLink style={{ textDecoration: "none" }}>
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
+                  Add
+                </Button>
+              </NavLink>
+            )}
+          </Box>
+
+          {isLogin ? (
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: { xs: "none", md: "flex" },
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 5,
+              }}
+            >
+              <NavLink
+                onClick={() => {
+                  localStorage.removeItem("userLogin");
+                  setLoading(true);
+                }}
+                style={{ textDecoration: "none" }}
+              >
+                <Button sx={{ my: 2, color: "white", display: "block" }}>
+                  Logout
+                </Button>
+              </NavLink>
+              <Typography
+                style={{
+                  textDecoration: "underline",
+                  background: "white",
+                  color: "blue",
+                  padding: 10,
+                  borderRadius: "5px",
+                }}
+              >
+                {isLogin.email}
+              </Typography>
+            </Box>
+          ) : (
+            <LoginGoogle setLoading={setLoading} />
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
+
+export default Navigation;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react';
-export default class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                copyright @ 2012
-            </div>
-        )
-    }
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function Footer() {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 60,
+        display: "flex",
+        backgroundColor: "#1976d2",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography
+        sx={{
+          color: "white",
+        }}
+      >
+        copyright @2022
+      </Typography>
+    </Box>
+  );
 }
+
+export default Footer;
